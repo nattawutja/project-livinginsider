159c6aa9ab854788d07d77e01d5cf65deffbb7d8
nattawutja <b.nattawut79@gmail.com>
2025-09-18 22:48:17 +0700

    test push inmacbook

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
                   <a href="#" className="inline-block  text-white hover:text-amber-500">ขายบ้าน</a>
                 </li>
                 <li>
-                  <a href="#" className="inline-block  text-white hover:text-amber-500">ขายที่ดินนoo</a>
+                  <a href="#" className="inline-block  text-white hover:text-amber-500">ขายที่ดินนoooo</a>
                 </li>
             </ul>
           </div>
