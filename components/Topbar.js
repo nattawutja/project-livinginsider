26d21200b3f25e6dee4309a1db120522504c8ff5
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-18 15:45:11 +0700

    add Topbar.js

--- components/Topbar.js
+++ components/Topbar.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function TopBar(){
     return(
-        <nav className="fixed top-0 left-0 w-full bg-black/50 backdrop-blur-md p-4 text-white z-50">
+        <nav className="fixed top-0 left-0 w-full z-50">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
                 <Image
@@ -12,25 +12,22 @@ export default function TopBar(){
                 height={50}
                 />
                 </a>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                    <ul className="text-xs flex flex-col mt-4 rounded-lg md:flex-row">
                         <li>
-                            <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                            <a href="#" className="px-5 py-2 inline-block  text-white hover:text-black rounded-full  transition duration-100 bg-green-400">Home</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                            <a href="#" className="px-5 py-2 inline-block  text-white hover:text-black rounded-md hover:rounded-full hover:bg-teal-100 transition duration-500">about</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                            <a href="#" className="px-5 py-2 inline-block  text-white hover:text-black rounded-md hover:rounded-full hover:bg-teal-100 transition duration-500">about</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                            <a href="#" className="px-5 py-2 inline-block  text-white hover:text-black rounded-md hover:rounded-full hover:bg-teal-100 transition duration-500">about</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                            <a href="#" className="px-5 py-2 inline-block  text-white hover:text-black rounded-md hover:rounded-full hover:bg-teal-100 transition duration-500">about</a>
                         </li>
                     </ul>
                 </div>
