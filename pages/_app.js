5745bf6671c8715e81747f1b0524db54989652ab
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-19 16:12:05 +0700

    import footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import TopBar from "../components/Topbar";
-
+import Footer from "../components/Footer";
 export default function App({ Component, pageProps }) {
     
   return( 
   <>
-    <TopBar/>
+    <TopBar />
     <Component {...pageProps} />
+    <Footer />
   </>
   );
 }
