af082e3add698558e480412a45ada6dc376b84da
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-19 16:10:21 +0700

    add component/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    
+  return (
+
+   <footer className="bottom-0 w-full text-sm text-center text-white bg-black border-t">
+      &copy; {new Date().getFullYear()} Project Livinginsider
+    </footer>
+
+
+  );
+}
